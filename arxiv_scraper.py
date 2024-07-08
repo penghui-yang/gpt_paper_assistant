4862cc90cf1c5f1d17cf0f70c8a3a7496df348d7
Penghui Yang <phyang.cs@gmail.com>
2024-07-08 11:51:37 +0800

    Temporary patches for holidays

--- arxiv_scraper.py
+++ arxiv_scraper.py
@@ -72,7 +72,7 @@ def get_papers_from_arxiv_api(area: str, timestamp, last_id) -> List[Paper]:
 
 def get_papers_from_arxiv_rss(area: str, config: Optional[dict]) -> List[Paper]:
     # get the feed from http://export.arxiv.org/rss/ and use the updated timestamp to avoid duplicates
-    updated = datetime.utcnow() - timedelta(days=1)
+    updated = datetime.utcnow() - timedelta(days=5)
     # format this into the string format 'Fri, 03 Nov 2023 00:30:00 GMT'
     updated_string = updated.strftime("%a, %d %b %Y %H:%M:%S GMT")
     feed = feedparser.parse(
