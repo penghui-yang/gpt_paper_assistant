81ad0d68e753d542f5b50fa6c63dd0cb9f3f8635
Penghui Yang <phyang.cs@gmail.com>
2024-10-02 12:05:08 +0800

    add the cost of gpt-4o gpt-4o-2024-08-06 and gpt-4o-mini

--- filter_papers.py
+++ filter_papers.py
@@ -56,6 +56,12 @@ def calc_price(model, usage):
         return (0.01 * usage.prompt_tokens + 0.03 * usage.completion_tokens) / 1000.0
     if model == "gpt-4":
         return (0.03 * usage.prompt_tokens + 0.06 * usage.completion_tokens) / 1000.0
+    if model == "gpt-4o":
+        return (0.005 * usage.prompt_tokens + 0.015 * usage.completion_tokens) / 1000.0
+    if model == "gpt-4o-2024-08-06":
+        return (0.0025 * usage.prompt_tokens + 0.01 * usage.completion_tokens) / 1000.0
+    if model == "gpt-4o-mini":
+        return (0.00015 * usage.prompt_tokens + 0.0006 * usage.completion_tokens) / 1000.0
     if (model == "gpt-3.5-turbo") or (model == "gpt-3.5-turbo-1106"):
         return (0.0015 * usage.prompt_tokens + 0.002 * usage.completion_tokens) / 1000.0
 
